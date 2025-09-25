de3f9ce20619ba2e81eb48261be8bb8ffbdef9d6
aymanebenomar <aymanebenomar2005@gmail.com>
2025-09-25 18:24:00 +0100

    main setup

---
+++ src/constants/index.js
@@ -0,0 +1,13 @@
+export const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+];
+
+export const words2 = [
+  { text: "Insights", imgPath: "//images/ideas.svg" },
+  { text: "Decisions", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+];
---
+++ src/sections/hero.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { words } from '../constants'
+import { words2 } from '../constants'
+import Button from '../components/Button'
+
+const hero = () => {
+  return (
+	<section id="hero" className="relative overflow-hidden" >
+		<div className="absolute top-0 left-0 z-10">
+			<img src='/images/bg.png' alt="Background" />
+		</div>
+
+		<div className='hero-layout'>
+		{/* LEFT HERO CONTENT */}
+
+		<header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+			<div className='flex flex-col gap-7'>
+				<div className='hero-text'>
+					<h1>
+						Turning Coffee Into 
+						<span className="slide">
+							<span className="wrapper">
+								{words.map((word, index) => (
+								<span
+									key={index}
+									className="flex items-center md:gap-3 gap-1 pb-2"
+								>
+									<img
+									src={word.imgPath}
+									alt="person"
+									className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+									/>
+									<span>{word.text}</span>
+								</span>
+								))}
+							</span>
+							</span>
+						</h1>
+					<h1>
+						And Data Into
+						<span className="slide">
+							<span className="wrapper">
+								{words2.map((word, index) => (
+								<span
+									key={index}
+									className="flex items-center md:gap-3 gap-1 pb-2"
+								>
+									<img
+									src={word.imgPath}
+									alt="person"
+									className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+									/>
+									<span>{word.text}</span>
+								</span>
+								))}
+							</span>
+							</span>
+					</h1>
+				</div>
+				<p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Benomar Aymane - Moroccan full-stack developer | data engineering student. I build things that work, fix things that don't, and yes, I actually read the documentation. Your project is safe with me.
+            </p>
+			<Button
+              text="See My Work"
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="counter"
+            />
+			</div>
+			</header>
+		</div>
+	</section>
+  )
+}
+
+export default hero
